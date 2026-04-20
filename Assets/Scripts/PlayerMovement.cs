3bbda0e09a57ab1bea9fb67b0a7a4c8c173e6d46
VicLatino7 <viclatino7@gmail.com>
2026-04-20 03:01:24 -0500

    Setup input
    Setting input and moving ship around clamped values

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float controlSpeed = 30f;
+    [SerializeField] float xClampRange = 23f;
+    [SerializeField] float yClampRange = 15f;
+
+
+    Vector2 movement;
+    void Update()
+    {
+        ProcessTranslation();
+    }
+
+    void OnMove(InputValue value)
+    {
+        Debug.Log("Move: " + value.Get<Vector2>());
+        movement = value.Get<Vector2>();
+    }
+    private void ProcessTranslation()
+    {
+        float xOffset = movement.x * controlSpeed * Time.deltaTime;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xClampRange, xClampRange);
+
+
+        float yOffset = movement.y * controlSpeed * Time.deltaTime;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yClampRange, yClampRange);
+        
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, 0f);
+    }
+}
