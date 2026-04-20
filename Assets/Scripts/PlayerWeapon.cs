6385fb9301fe3c726aee219b81136b81656fd68a
VicLatino7 <viclatino7@gmail.com>
2026-04-20 04:16:22 -0500

    connect input to laser emission
    -appended new action to action map
    -created Player Weapon script
    -updated 'Fire' action with a Press interaction and trigger behavior set to press and release
    -serialized field for laser game object
    -added logic to toggle emission

---
+++ Assets/Scripts/PlayerWeapon.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerWeapon : MonoBehaviour
+{
+    [SerializeField] GameObject laser;
+    bool isFiring = false;
+
+    void Update()
+    {
+        ProcessFiring();
+    }
+    void OnFire(InputValue value)
+    {
+        isFiring = value.isPressed;
+    }
+
+    void ProcessFiring()
+    {
+        var emmissionModule = laser.GetComponent<ParticleSystem>().emission;
+        emmissionModule.enabled = isFiring;
+    }
+}
