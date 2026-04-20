822b2e0cae6ae413f3888c9cfa38f3ff19279c92
VicLatino7 <viclatino7@gmail.com>
2026-04-20 03:23:37 -0500

    Finish input
    Completed player input and proper movement which includes moving in the x and y axis and implementing rotation using quaternions and lerp

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,13 +5,19 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float controlSpeed = 30f;
     [SerializeField] float xClampRange = 23f;
-    [SerializeField] float yClampRange = 15f;
+    [SerializeField] float yClampRange = 13f;
+
+    [SerializeField] float controlPitchFactor = 20f;
+    [SerializeField] float controlRollFactor = 20f;
+    [SerializeField] float rotationSpeed = 10f;
+
 
 
     Vector2 movement;
     void Update()
     {
         ProcessTranslation();
+        ProcessRotation();
     }
 
     void OnMove(InputValue value)
@@ -29,7 +35,15 @@ public class PlayerMovement : MonoBehaviour
         float yOffset = movement.y * controlSpeed * Time.deltaTime;
         float rawYPos = transform.localPosition.y + yOffset;
         float clampedYPos = Mathf.Clamp(rawYPos, -yClampRange, yClampRange);
-        
+
         transform.localPosition = new Vector3(clampedXPos, clampedYPos, 0f);
+    }
+    private void ProcessRotation()
+    {
+        float pitch = -controlPitchFactor * movement.y;
+        float roll = -controlRollFactor * movement.x;
+
+        Quaternion targetRotation = Quaternion.Euler(pitch, 0f, roll);
+        transform.localRotation = Quaternion.Lerp(transform.localRotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
 }
