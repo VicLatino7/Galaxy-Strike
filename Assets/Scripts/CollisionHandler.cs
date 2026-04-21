23e5f1d25c357f231a85c1dfce705d4cbbcf78ad
VicLatino7 <viclatino7@gmail.com>
2026-04-21 02:14:34 -0500

    player explosion VFX
    -creating prefab variant for player explosion (green)
    -added logic to collision handler for player and assigned player VFX to serialized field

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -2,20 +2,11 @@ using UnityEngine;
 
 public class CollisionHandler : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] GameObject destroyedVFX;
 
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Collided with: " + other.gameObject.name);
+        Instantiate(destroyedVFX, this.transform.position, Quaternion.identity);
+        Destroy(this.gameObject);
     }
 }
