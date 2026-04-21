0ca3b39bbfae06a01a2d460ff1b512fc6b8fb6e0
VicLatino7 <viclatino7@gmail.com>
2026-04-20 23:50:21 -0500

    instantiation of particle effects
    -instantiating particle system for "blown up" enemy ships
    -creating particle system (VFX) for destroyed ships
    -inserted logic into Enemy script
    -inserted instantiation of particle effects and unchecked Looping so VFX only plays once
    
    note: initial enemy on timeline is broken

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,7 +2,10 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    private void OnParticleCollision(GameObject other) {
+    [SerializeField] GameObject destroyedVFX;
+    private void OnParticleCollision(GameObject other) 
+    {
+        Instantiate(destroyedVFX, this.transform.position, Quaternion.identity);
         Destroy(this.gameObject);
     }
 }
