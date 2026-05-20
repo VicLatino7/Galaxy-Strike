9623f44b68667e4f598a46f46069c98fc0a3ae1d
VicLatino7 <viclatino7@gmail.com>
2026-05-20 00:11:38 -0500

    Enemy Health and Scoreboard
    Implemented enemy health and a scoreboard

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,6 +4,13 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject destroyedVFX;
     [SerializeField] int hitPoints = 3;
+    [SerializeField] int scoreValue = 10;
+
+    Scoreboard scoreboard;
+    private void Start() 
+    {
+        scoreboard = FindFirstObjectByType<Scoreboard>();
+    }
     private void OnParticleCollision(GameObject other) 
     {
         ProcessHit();
@@ -14,6 +21,7 @@ public class Enemy : MonoBehaviour
         hitPoints--;
         if (hitPoints <= 0)
         {
+            scoreboard.IncreaseScore(scoreValue);
             Instantiate(destroyedVFX, this.transform.position, Quaternion.identity);
             Destroy(this.gameObject);
         }
